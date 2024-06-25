5564c22b71904a2d72ede1270cc3f421351ec7b7
wln20 <wangluning2@gmail.com>
2024-06-25 10:51:50 +0800

    [minor] cchangefile naming regulation

--- svdkv_src/utils/general_utils.py
+++ svdkv_src/utils/general_utils.py
@@ -3,7 +3,7 @@ General purpose utils
 """
 
 def get_name(args):
-    return f"density_k_{args.k_density}_v_{args.v_density}_quant_k_{args.k_bits}_v_{args.v_bits}_{'asvd_' if args.use_asvd else ''}{f'window_{args.q_window_size}_' if args.use_window else ''}"
+    return f"density_k_{args.k_density}_v_{args.v_density}_quant_k_{args.k_bits}_v_{args.v_bits}_{'asvd_' if args.use_asvd else ''}{f'window_{args.q_window_size if not (args.k_bits == 16 and args.v_bits == 16) else 0}_' if args.use_window else ''}"
 
 def check_params(args):
     assert ((args.k_density < 1.0 and args.k_density > 0.0) and (args.v_density < 1.0 and args.v_density > 0.0)), \
